2263f68dce1a5ab43daaed31bc825de0b62a6a7d
Trigit-hub <cjtr.1998@gmail.com>
2022-06-23 21:56:15 -0600

    avanzando

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,70 @@
-import Head from 'next/head'
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import { DrawerProvider } from '../common/contexts/DrawerContext';
+import Sticky from 'react-stickynode';
+import { foodDeliveryTheme } from '../common/theme/foodDelivery';
+import ResetCSS from '../common/assets/css/style';
+import {
+  GlobalStyle,
+  ContentWrapper,
+} from '../containers/FoodDelivery/foodDelivery.style';
 
-export default function Home() {
+import Navbar from '../containers/FoodDelivery/Navbar';
+import Testimonials from '../containers/FoodDelivery/Testimonials';
+import Banner from '../containers/FoodDelivery/Banner';
+import AvailableRestaurants from '../containers/FoodDelivery/AvailableRestaurants';
+import PopularCategory from '../containers/FoodDelivery/PopularCategory';
+import HowWorks from '../containers/FoodDelivery/HowWorks';
+import ProductDelivery from '../containers/FoodDelivery/ProductDelivery';
+import QualityFeatures from '../containers/FoodDelivery/QualityFeatures';
+import Clients from '../containers/FoodDelivery/Clients';
+import DownloadApp from '../containers/FoodDelivery/DownloadApp';
+import Gallery from '../containers/FoodDelivery/Gallery';
+import CallToAction from '../containers/FoodDelivery/CallToAction';
+import Footer from '../containers/FoodDelivery/Footer';
+
+const FoodDelivery = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
+    <ThemeProvider theme={foodDeliveryTheme}>
+      <Fragment>
+        <Head>
+          <title>FoodDelivery | A react next landing page</title>
+          <meta name="Description" content="React next landing page" />
+          <meta name="theme-color" content="#fff2d9" />
+          {/* Load google fonts */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <ResetCSS />
+        <GlobalStyle />
+        <ContentWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <Banner />
+          <AvailableRestaurants />
+          <PopularCategory />
+          <HowWorks />
+          <ProductDelivery />
+          <QualityFeatures />
+          <Clients />
+          <Testimonials />
+          <DownloadApp />
+          <Gallery />
+          <CallToAction />
+          <Footer />
+        </ContentWrapper>
+      </Fragment>
+    </ThemeProvider>
+  );
+};
+export default FoodDelivery;
